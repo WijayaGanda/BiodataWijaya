f1f9cbf0faa110c9fec25912871578fba70c41e1
WijayaGanda <64698425+WijayaGanda@users.noreply.github.com>
2020-08-19 13:14:49 +0700

    Wijaya Ganda prasetyo

---
+++ src/com/Wijaya/BiodataWijaya.java
@@ -0,0 +1,10 @@
+package com.Wijaya;
+
+public class BiodataWijaya {
+    public static void main(String[] args) {
+        System.out.println("Nama Lengkap : Wijaya Ganda Prasetyo");
+        System.out.println("Jenis Kelamin : Laki-Laki");
+        System.out.println("Alamat : Desa Suwayuwo, Kec Sukorejo, Kab Pasuruan");
+        System.out.println("Jurusan : Rekayasa Perangkat lunak (RPL)");
+    }
+}
